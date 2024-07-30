6130a7ae974ac8844a57867359e38740739a9aa0
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-07-30 07:46:24 -0700

    string reverse

---
+++ reverse.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* rev_str - a function that reverses the string
+* @s: the  string to be reversed
+* Return: count of characers that are printed printed
+*/
+int  rev_str(char *s)
+{
+	int x, y = 0;
+
+	while (s[y])
+		j++;
+
+	for (x = y - 1; x >= 0; x--)
+	{
+		_putchar(s[x]);
+	}
+	return (y);
+}
