c877506af522db45a8ac3b589efb372eeb85627d
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-07-30 07:24:37 -0700

    add mmain.c

--- printf_bin.c
+++ printf_bin.c
@@ -1,34 +1,17 @@
 #include "main.h"
 
 /**
- * printf_bin - prints a binary number.
- * @val: arguments.
- * Return: 1.
+ * main - Entry point
+ *
+ * Return: Always 0
  */
-int printf_bin(va_list val)
+int main(void)
 {
-	int flag = 0;
-	int cont = 0;
-	int i, a = 1, b;
-	unsigned int num = va_arg(val, unsigned int);
-	unsigned int p;
+    _printf("%b\n", 98);
+    _printf("%b\n", 97);
+    _printf("%b\n", 95557);
+    _printf("%b\n", 912222227);
 
-	for (i = 0; i < 32; i++)
-	{
-		p = ((a << (31 - i)) & num);
-		if (p >> (31 - i))
-			flag = 1;
-		if (flag)
-		{
-			b = p >> (31 - i);
-			_putchar(b + 48);
-			cont++;
-		}
-	}
-	if (cont == 0)
-	{
-		cont++;
-		_putchar('0');
-	}
-	return (cont);
+    _printf("%b\n", 4);
+    return (0);
 }
