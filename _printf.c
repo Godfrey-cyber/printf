7e8cd7452004e0f4fc51f99311ea2a1195c48832
New Name <godfreyndiritu26@gmail.com>
2024-09-12 23:58:23 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,65 +1,45 @@
-#include <stdarg.h>
-#include <unistd.h>
 #include "main.h"
-
 /**
- * _printf - Custom implementation of printf function.
- * @format: Format string containing the characters and the specifiers.
- *
- * Description: This function produces output according to a format.
- * It handles the following conversion specifiers: c, s, and %.
- *
- * Return: The number of characters printed (excluding the null byte).
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
  */
-int _printf(const char *format, ...)
+int _printf(const char * const format, ...)
 {
-    va_list args;
-    int i = 0, count = 0;
-    char *str;
-    char ch;
+	convert_match m[] = {
+		{"%s", printf_string}, {"%c", printf_char},
+		{"%%", printf_37},
+		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
+		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
+		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
+		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
+	};
 
-    if (format == NULL)
-        return (-1);
+	va_list args;
+	int i = 0, j, len = 0;
 
-    va_start(args, format);
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
 
-    while (format && format[i])
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            switch (format[i])
-            {
-                case 'c':
-                    ch = (char)va_arg(args, int);
-                    count += write(1, &ch, 1);
-                    break;
-                case 's':
-                    str = va_arg(args, char *);
-                    if (str == NULL)
-                        str = "(null)";
-                    while (*str)
-                    {
-                        count += write(1, str, 1);
-                        str++;
-                    }
-                    break;
-                case '%':
-                    count += write(1, "%", 1);
-                    break;
-                default:
-                    count += write(1, "%", 1);
-                    count += write(1, &format[i], 1);
-                    break;
-            }
-        }
-        else
-        {
-            count += write(1, &format[i], 1);
-        }
-        i++;
-    }
-
-    va_end(args);
-    return (count);
+Here:
+	while (format[i] != '\0')
+	{
+		j = 13;
+		while (j >= 0)
+		{
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			{
+				len += m[j].f(args);
+				i = i + 2;
+				goto Here;
+			}
+			j--;
+		}
+		_putchar(format[i]);
+		len++;
+		i++;
+	}
+	va_end(args);
+	return (len);
 }
