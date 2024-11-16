bf58977e507fe5d87c4f49ebc725ab242ad41a3b
New Name <godfreyndiritu26@gmail.com>
2024-11-16 08:06:01 -0800

    write_handlers.c

--- get_flags.c
+++ get_flags.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
- * 
- * Return: Flags:
- */
+   * get_flags - Calculates active flags
+   * @format: Formatted string in which to print the arguments
+   * @i: take a parameter.
+   * Return: Flags:
+   */
 int get_flags(const char *format, int *i)
 {
 	/* - + 0 # ' ' */
@@ -24,7 +23,6 @@ int get_flags(const char *format, int *i)
 				flags |= FLAGS_ARR[j];
 				break;
 			}
-
 		if (FLAGS_CH[j] == 0)
 			break;
 	}
